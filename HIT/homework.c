8ddc4917312faeebf217fdcace5305280e20433e
fan <13872564585@163.com>
2022-09-18 14:21:06 +0800

    modified:   HIT/homework.c 	modified:   coin/homework.exe

--- HIT/homework.c
+++ HIT/homework.c
@@ -3,37 +3,48 @@
 #include <math.h>
 int main()
 {
-    int max = -1, cnt = 0;
+    int max = -1, cnt = 0, a, b, c;
     for (int i = 0; i < 1430; i++)
-        for (int j = 0; j < ceil((10000 - 7 * i) / 5); j++)
-            for (int k = 0; k < ceil((10000 - 7 * i - j * 5) / 4); k++)
+        for (int j = 0; j <= (10000 - 7 * i) / 5; j++)
+            for (int k = 0; k <= (10000 - 7 * i - j * 5) / 4; k++)
             {
                 if (i * 7 + j * 5 + k * 4 == 10000)
+                {
                     cnt++;
-                if (i * 0.4 + j * 0.56 + k * 0.48 > max)
-                    max = i * 0.4 + j * 0.56 + k * 0.48;
+                    if (i * 0.4 + j * 0.56 + k * 0.48 > max)
+                    {
+                        max = i * 0.4 + j * 0.56 + k * 0.48;
+                        a = i;
+                        b = j;
+                        c = k;
+                    }
+                }
             }
-    printf("%d\n%d",cnt,max);
+    printf("there are %d combinations in  total\nA=%4dB=%4dC=%4d\nbenifit=%d\n", cnt, a, b, c, max);
 
-    int n=0;
-    for(int i=1;i<=10;i++){
-        n*=2;
+    int n = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        n *= 2;
         n++;
     }
-    printf("%d\n",n);
+    printf("there will be %d cells after 10 days\n", n);
 
-    int x=2;
-    for(int i=1;i<=7;i++){
+    int x = 2;
+    for (int i = 7; i >= 1; i--)
+    {
         x++;
-        x*=2;
+        x *= 2;
+        printf("sell %d ducks at day %d\n", x / 2 + 1, i);
     }
-    printf("%d\n",x);
+    printf("initially he had %d ducks\n", x);
 
-    int a=10;
-    for(int i=1;i<=4;i++){
-        a+=2;
+    a = 10;
+    for (int i = 1; i <= 4; i++)
+    {
+        a += 2;
     }
-    printf("%d",a);
+    printf("the fifth person is %d years old\n", a);
 
     system("pause");
     return 0;
