dd227d4f952b854dfa6f78b9e0f00a981a139eed
fan <13872564585@163.com>
2022-09-23 09:59:43 +0800

    modified:   HIT/9.21/1.c 	modified:   HIT/pr.c 	modified:   coin/1.exe 	modified:   coin/pr.exe

--- HIT/9.21/1.c
+++ HIT/9.21/1.c
@@ -11,29 +11,30 @@ int qb(int a)
     else
         return qb(a - 1) * 2 + 1;
 }
-int qc(int a){
-    if(a==7)
-    return 2;
-    else 
-    return (qc(a+1)+1)*2;
-}
-int qd(int a){
-    if(a==1)
-    return 10;
+int qc(int a)
+{
+    if (a == 8)
+        return 2;
     else
-    return qd(a-1)+2;
+        return (qc(a + 1) + 1) * 2;
+}
+int qd(int a)
+{
+    if (a == 1)
+        return 10;
+    else
+        return qd(a - 1) + 2;
 }
 int main()
 {
     int a, b;
     scanf("%d%d", &a, &b);
     printf("gcd=%d\n", gcd(a, b));
-
     printf("%d\n", qb(10));
-
-    printf("%d\n",qc(1));
-
-    printf("%d\n",qd(5));
+    printf("%d\n", qc(1));
+    for (int i = 2; i <= 8; i++)
+        printf("sold %d ducks at day %d\n", qc(i) + 2, i - 1);
+    printf("%d\n", qd(5));
     system("pause");
     return 0;
 }
--- HIT/pr.c
+++ HIT/pr.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-int n,flag;
-int mkrandom();
-int guess(int n,int x);
-int main(){
-    do{
-        scanf("%d",&n);
-        guess(n,mkrandom());
-    }while(!flag);
+char c;
+int a[30];
+int main()
+{
+    while ((c = getchar())&&c != EOF)
+    {
+        for (int i = 0; i < 26; i++)
+            if ((int)c == i + 97)
+                a[i]++;
+    }
+    for (int i = 0; i < 26; i++)
+        if (a[i] == 0)
+            continue;
+        else
+            printf("%c=%d\n", (int)(i + 97), a[i]);
     system("pause");
     return 0;
-}
-int mkrandom(){
-    srand(time(0));
-    return rand();
-}
-int guess(int n,int x){
-    x = mkrandom();
-    if(n>x)
-        printf("too big!\n");
-    else if(n<x)
-        printf("Too small!\n");
-    else
-        flag =1;
 }
